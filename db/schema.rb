9574b5263ec9844480edc8e1d366b23b87964aa2
naoey <naoey@protonmail.ch>
2019-07-01 17:04:34 +0530

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -61,8 +61,8 @@ ActiveRecord::Schema.define(version: 2019_07_01_082016) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "email"
+    t.string "encrypted_password"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
