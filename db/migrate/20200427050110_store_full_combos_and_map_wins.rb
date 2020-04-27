e9e96fc2a63c33fa66c0e8f8fa7e4e36c2eeb96b
naoey <naoey@protonmail.ch>
2020-04-27 11:25:23 +0530

    Add migration for new columns and migrate old data

---
+++ db/migrate/20200427050110_store_full_combos_and_map_wins.rb
@@ -0,0 +1,38 @@
+class StoreFullCombosAndMapWins < ActiveRecord::Migration[6.0]
+  def up
+    change_table :match_scores do |t|
+      t.column :is_full_combo, :boolean
+      t.column :is_win, :boolean
+
+      MatchScore.all.each do |score|
+        # find this player's team id
+        if score.match.red_team.players.include?(score.player)
+          player_team = score.match.red_team
+          other_team = score.match.blue_team
+        else
+          player_team = score.match.blue_team
+          other_team = score.match.red_team
+        end
+
+        # find both teams total scores
+        player_team_score = MatchScore
+          .where({ player: player_team.players, match: score.match })
+          .sum(:score)
+
+        other_team_score = MatchScore
+          .where({ player: other_team.players, match: score.match })
+          .sum(:score)
+
+        score.update(
+          is_win: player_team_score > other_team_score,
+          is_full_combo: score.count_miss.zero? && (score.beatmap.max_combo - score.max_combo) <= 0.01 * score.beatmap.max_combo
+        )
+      end
+    end
+  end
+
+  def down
+    remove_column :match_scores, :is_win
+    remove_column :match_scores, :is_win
+  end
+end
