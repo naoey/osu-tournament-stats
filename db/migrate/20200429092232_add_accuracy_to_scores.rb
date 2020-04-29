53580504081862a0c9481722a6ebfe67379d740f
naoey <naoey@protonmail.ch>
2020-04-29 15:52:45 +0530

    Add accuracy column to scores

---
+++ db/migrate/20200429092232_add_accuracy_to_scores.rb
@@ -0,0 +1,31 @@
+class AddAccuracyToScores < ActiveRecord::Migration[6.0]
+  def up
+    change_table :match_scores do |t|
+      t.column :accuracy, :float
+    end
+
+    # calculate accuracies for all existing scores
+    MatchScore.where(accuracy: nil).all.each do |score|
+      acc = AccuracyHelper.calculate_accuracy(score)
+
+      raise ArgumentError, "Accuracy for score with ID #{score.id} is null, migration failed" if acc.nil?
+
+      score.update(accuracy: acc)
+
+      if MatchScore.find(score.id).accuracy.nil?
+        raise ArgumentError, "Accuracy for score with ID #{score.id} is null after update, migration failed"
+      end
+    end
+
+    failed_scores = MatchScore.where(accuracy: nil).select('id').all
+
+    raise ArgumentError, "Accuracy is null for scores #{failed_scores.map(&:id)}; migration failed!" unless failed_scores.empty?
+
+    # set accuracy to be non-null
+    change_column :match_scores, :accuracy, :float, null: false
+  end
+
+  def down
+    remove_column :match_scores, :accuracy
+  end
+end
