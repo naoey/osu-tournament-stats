6702dd6baa186e0dbce02b1745522d9cf61494ee
naoey <naoey@protonmail.ch>
2020-04-29 17:02:29 +0530

    Remove autogenerated test file and add accuracy to score factory

--- test/controllers/beatmaps_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class BeatmapsControllerTest < ActionDispatch::IntegrationTest
-  test "should get get" do
-    get beatmaps_get_url
-    assert_response :success
-  end
-
-end
