796c3d77d92fded6e46a0398555322e73ffe1218
naoey <naoey@protonmail.ch>
2020-05-05 13:58:16 +0530

    Fix discord bot showing arrays instead of counting them

--- lib/discord/modules/leaderboard_commands.rb
+++ lib/discord/modules/leaderboard_commands.rb
@@ -5,7 +5,7 @@ require_relative '../commands/score_leaderboard'
 module LeaderboardCommands
   extend Discordrb::Commands::CommandContainer
 
-  command(:score_leaderboard, aliases: [:slb], description: 'View player leaderboard') do |event, *args|
+  command(:leaderboard, aliases: [:lb], description: 'View player leaderboard') do |event, *args|
     ScoreLeaderboard.new(event, *args).response
   end
 end
