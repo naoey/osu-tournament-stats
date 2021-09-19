ef3112af6b982e58a3431346abf5339a372e50e6
naoey <naoey@protonmail.ch>
2021-09-19 18:24:15 +0530

    Change to plain text browser response and log alt verify attempts

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -9,10 +9,10 @@ class AuthController < ApplicationController
 
       player = Player.find_by(osu_id: osu_user['id'])
 
-      if !player.nil? && player != auth_request.player
-        # If a player was found with this osu! ID but is not the player who invoked this auth request, it's like a player created
-        # through an osu! match import and is not linked to this Discord user. Link them here and delete the new user that was created
-        # by the Discord command if any
+      if !player.nil? && player.discord_id.nil?
+        # If a player was found with this osu! ID but whose discord ID is empty, then the osu! user was likely already added earlier
+        # through a match import. Link this authorising discord user to that existing player and delete the new user created by
+        # the discord command.
         dummy_player = auth_request.player
 
         player.discord_id = dummy_player.discord_id
@@ -21,19 +21,29 @@ class AuthController < ApplicationController
         player.save!
         auth_request.save!
         dummy_player.destroy!
+
+        auth_request.player.complete_osu_verification(params[:state], osu_user)
+      elsif !player.nil? && !player.discord_id.nil? && player.discord_id != auth_request.player.discord_id
+        # If a player was found with this osu! ID but whose discord ID doens't match the existing discord ID, they are trying to verify
+        # an alternate discord ID and should not be allowed.
+
+        ActiveSupport::Notifications.instrument 'player.alt_discord_verify', { auth_request: auth_request, player: player }
+
+        return render plain: 'Verification failed. This osu! account is already linked to another Discord ID. Contact the server admins if you have a valid reason for using a new Discord account.',
+                      status: :ok
       end
 
       auth_request.player.complete_osu_verification(params[:state], osu_user)
 
-      render json: { message: 'Verification successful. Contact the Discord server administrators if you still do not have access.' },
+      render plain: 'Verification successful. Contact the Discord server administrators if you still do not have access.',
              status: :ok
     rescue OsuAuthErrors::TimeoutError => e
-      render json: { message: e.message }, status: :bad_request
+      render plain: e.message, status: :bad_request
     rescue OsuAuthErrors::OsuAuthError => e
-      render json: { message: e.message }, status: :bad_request
+      render plain: e.message, status: :bad_request
     rescue StandardError => e
       logger.error(e)
-      render json: { message: 'An unknown error occurred' }, status: :internal_server_error
+      render plain: 'An unknown error occurred', status: :internal_server_error
     end
   end
 end
