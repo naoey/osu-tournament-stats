9a1973d93f9e4a419be7b8e62e8fd3ad25c8f710
naoey <naoey@protonmail.ch>
2021-09-21 13:57:07 +0530

    Add a command to retrieve osu! profile of mentioned user

---
+++ lib/discord/commands/registration/whois.rb
@@ -0,0 +1,31 @@
+require_relative '../command_base'
+
+class Whois < CommandBase
+  protected
+
+  def make_response
+    if @event.message.mentions.length != 1
+      return @event.respond('Whois requires one mentioned user')
+    end
+
+    target = @event.message.mentions.first
+    player = Player.find_by(discord_id: target.id)
+
+    return @event.respond("User #{target.name} not found") if player.nil?
+
+    @event.message.channel.send_embed do |embed|
+      embed.title = player.name
+      embed.url = "https://osu.ppy.sh/users/#{player.osu_id}"
+      embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: "https://a.ppy.sh/#{player.osu_id}")
+      embed.color = EMBED_GREEN
+      embed.fields = [
+        Discordrb::Webhooks::EmbedField.new(name: 'Discord user', value: "<@#{player.discord_id}>", inline: true),
+        Discordrb::Webhooks::EmbedField.new(name: 'osu! ID', value: player.osu_id, inline: true),
+        Discordrb::Webhooks::EmbedField.new(
+          name: 'Verified on',
+          value: player.osu_auth_request&.updated_at.change(offset: "+05:30").to_formatted_s(:long) || 'Never'
+        )
+      ]
+    end
+  end
+end
