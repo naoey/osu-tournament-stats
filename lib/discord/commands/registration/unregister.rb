5ec57232c4f758f16f463aa8fb9df890f9c358f5
naoey <naoey@protonmail.ch>
2022-05-21 12:03:01 +0530

    Add option to unregister users with osu ID param

--- lib/discord/commands/registration/unregister.rb
+++ lib/discord/commands/registration/unregister.rb
@@ -9,25 +9,34 @@ class Unregister < CommandBase
     true
   end
 
+  def required_options
+    [
+      ['-i TEXT', '--osu_id', 'The osu! ID of the player to disassociate from their Discord ID.']
+    ]
+  end
+
   def make_response
-    if @event.message.mentions.length == 0
+    osu_id = @options[:osu_id]
+
+    if osu_id.nil? && @event.message.mentions.length == 0
       return @event.respond('No user specified to unregister')
     end
 
-    if @event.message.mentions.length > 1
-      return @event.respond('Mention a single user to unregister')
+    if osu_id.nil? && @event.message.mentions.length > 0
+      member = @server[:discordrb_server].member(@event.message.mentions.first.id)
     end
 
-    mentioned_member = @event.message.server.member(@event.message.mentions.first.id)
-
-    return @event.respond('Mentioned user is not in the server') if mentioned_member.nil?
-
-    player = Player.find_by(discord_id: mentioned_member.id)
-    server = DiscordServer.find_by(discord_id: @event.message.server.id)
-
-    if player.nil?
-      return @event.respond("User #{mentioned_member.name} is not registered")
+    player = osu_id.nil? ? Player.find_by(discord_id: member.id) : Player.find_by(osu_id: osu_id.to_i)
+    
+    if player.nil? || player.discord_id.nil?
+      return @event.respond('osu! user has not registered a Discord ID.')
     end
+
+    if !player.osu_verified || player.osu_id.nil?
+      return @event.respond('User has not registered an osu! account.')
+    end
+
+    member ||= @server[:discordrb_server].member(player.discord_id)
 
     ActiveRecord::Base.transaction do
       player.discord_id = nil
@@ -35,11 +44,14 @@ class Unregister < CommandBase
 
       player.save!
 
-      if mentioned_member.role?(server.verified_role_id)
-        mentioned_member.remove_role(server.verified_role_id, "Unregister invoked by #{@event.message.author.name} (#{@event.message.author.id})")
+      if !member.nil? && member.role?(@server[:db_server].verified_role_id)
+        member.remove_role(
+          @server[:db_server].verified_role_id,
+          "Unregister invoked by #{@event.message.author.name} (#{@event.message.author.id})",
+        )
       end
 
-      @event.respond("Succesfully unregistered #{mentioned_member.name}")
+      @event.respond("Successfully unregistered #{player.name}")
     end
   end
 end
