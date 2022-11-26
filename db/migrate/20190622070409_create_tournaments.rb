3afe81ff1cb2de2524ae009ed2a711d946e2d234
naoey <naoey@protonmail.ch>
2022-11-26 18:18:28 +0530

    Fix incorrect `add_foreign_key` call in migration

--- db/migrate/20190622070409_create_tournaments.rb
+++ db/migrate/20190622070409_create_tournaments.rb
@@ -1,5 +1,5 @@
 class CreateTournaments < ActiveRecord::Migration[5.2]
-  def change
+  def up
     create_table :tournaments do |t|
       t.string :name, null: false
       t.bigint :host_player_id
@@ -11,17 +11,25 @@ class CreateTournaments < ActiveRecord::Migration[5.2]
 
     add_column :matches, :tournament_id, :bigint
 
-    add_foreign_key :matches, :tournaments, {
+    add_foreign_key(:matches, :tournaments,
       column: 'tournament_id',
       # primary_key: 'online_id',
       on_update: :cascade,
       on_delete: :cascade,
-    }
+    )
 
-    add_foreign_key :tournaments, :players, {
+    add_foreign_key(:tournaments, :players,
       column: 'host_player_id',
       on_update: :cascade,
       on_delete: :nullify,
-    }
+    )
+  end
+
+  def down
+    remove_foreign_key(:matches, :tournaments, { column: 'tournament_id' })
+    remove_foreign_key(:tournaments, :players, { column: 'host_player_id' })
+
+    remove_column(:matches, :tournament_id)
+    drop_table(:tournaments)
   end
 end
