f6b8cbaeb982e2e76ded45efac6af0ec97632d47
naoey <naoey@protonmail.ch>
2022-12-04 21:24:07 +0530

    Update models and relations for adding exp to players

--- app/models/discord_exp.rb
+++ app/models/discord_exp.rb
@@ -1,4 +1,27 @@
+require_relative '../helpers/exp_helper'
+
 class DiscordExp < ApplicationRecord
   belongs_to :player, optional: false
   belongs_to :discord_server, optional: false
+
+  def add_exp()
+    raise RuntimeError, "Cannot update exp within 60s of last update!" if Time.now - self.updated_at < 60.seconds
+
+    exp = rand(15..25)
+
+    current = self.detailed_exp[0]
+    to_next_level = self.detailed_exp[1]
+
+    if current + exp > to_next_level
+      self.detailed_exp[0] = 0
+      self.detailed_exp[1] = ExpHelper.exp_to_next_level?(self.level)
+      self.detailed_exp[2] = self.exp = self.detailed_exp[2] + exp
+      self.level += 1
+    else
+      self.detailed_exp[0] += exp
+      self.detailed_exp[2] = self.exp = self.detailed_exp[2] + exp
+    end
+
+    self.save!
+  end
 end
