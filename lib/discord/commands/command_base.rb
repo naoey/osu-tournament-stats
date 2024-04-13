608e5609c953bab9136b9426a12bba5e4ccb83c4
naoey <naoey@protonmail.ch>
2024-04-13 13:08:51 +0900

    Refactor `command_base` to for slash commands

--- lib/discord/commands/command_base.rb
+++ lib/discord/commands/command_base.rb
@@ -1,37 +1,19 @@
 require 'optparse'
 
 class CommandBase
-  def initialize(event, *args)
+  def initialize(bot, event, *args)
+    @bot = bot
     @event = event
     @bot_args = args
-    @server = DiscordServer.find_by_discord_id(event.message.server.id)
-    @invoker = PlayerAuth.find_by(uid: event.message.author.id, provider: :discord)&.player
-
-    @options = {}
-
-    begin
-      OptionParser.new do |opts|
-        required_options.each do |opt|
-          opts.on(*opt)
-        end
-
-        opts.on('-h', '--help', 'Prints this help message') do
-          @help_message = opts
-        end
-      end.parse!(event.message.content.split(' '), into: @options)
-    rescue OptionParser::MissingArgument
-      @event.respond("Missing arguments")
-      raise
-    end
+    @server = DiscordServer.find_by_discord_id(event.server.id)
+    @player = PlayerAuth.find_by(uid: event.interaction.user.id, provider: :discord)&.player
   end
 
-  def response
-    return "```#{@help_message}```" if @options[:help]
-
-    return 'Can only be invoked by adminstrators!' if requires_admin && !invoker_admin?
+  def respond
+    @event.respond(content: 'Can only be used by staff!', ephemeral: true) if requires_admin? && !invoker_admin?
 
     begin
-      make_response
+      handle_response
     rescue Discordrb::Errors::NoPermission
     end
   end
@@ -39,22 +21,22 @@ class CommandBase
   protected
 
   def mention_invoker
-    @event.message.author.mention
+    @event.interaction.user.mention
   end
 
-  def requires_admin
+  def requires_admin?
     false
   end
 
   def invoker_admin?
-    @event.message.author.defined_permission?(:administrator) || invoker_owner?
+    event.interaction.user.defined_permission?(:administrator) || invoker_owner?
   end
 
   def invoker_owner?
-    @event.message.author.owner?
+    event.interaction.user.owner?
   end
 
-  def make_response
+  def handle_response
     nil
   end
 
