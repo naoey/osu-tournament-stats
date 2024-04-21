790d38b15b01800e4e15854ec33e6a9dbd4cd369
naoey <naoey@protonmail.ch>
2024-04-21 14:05:40 +0900

    Remove old leaderboard commands

--- lib/discord/modules/leaderboard_commands.rb
+++ lib/discord/modules/leaderboard_commands.rb
@@ -1,16 +1,11 @@
 require "discordrb"
 
 require_relative "../commands/score_leaderboard"
-require_relative "../commands/exp/exp_leaderboard"
 
 module LeaderboardCommands
   extend Discordrb::Commands::CommandContainer
 
   command(:leaderboard, aliases: [:lb], description: "View player leaderboard") do |event, *args|
     ScoreLeaderboard.new(event, *args).response
-  end
-
-  command(:exp_leaderboard, aliases: [:xp_lb], description: "View Discord exp leaderboard") do |event, *args|
-    ExpLeaderboard.new(event, *args).response
   end
 end
