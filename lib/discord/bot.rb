7e8451a8912fa7dd65291e26c7c20939431a2b90
naoey <naoey@protonmail.ch>
2024-06-27 18:32:08 +0900

    Streamline registration flow and remove redundant guardrails

--- lib/discord/bot.rb
+++ lib/discord/bot.rb
@@ -135,23 +135,33 @@ module Discord
     end
 
     def osu_verification_completed(data)
-      servers = DiscordServer.where.not(verification_log_channel_id: nil)
-
       player = data[:player]
+      servers = DiscordServer
+        .where(discord_id: [client.servers.keys])
+        .and(DiscordServer.where.not(verification_log_channel_id: nil))
 
       servers.each do |server|
-        guild = @client.server(server.discord_id)
+        guild = begin
+          @client.server(server.discord_id)
+        rescue
+          next
+        end
+
         member = @client.member(server.discord_id, player.discord.uid)
 
         next if guild.nil? || member.nil? # Bot probably removed from server
 
-        unless server.verified_role_id.nil?
-          member.add_role(server.verified_role_id, "osu! verification completed with ID #{player.osu.uid}")
-          member.set_nick(player.osu.uname, "osu! user #{player.osu.uname} linked")
-        end
+        begin
+          unless server.verified_role_id.nil?
+            member.add_role(server.verified_role_id, "osu! verification completed with ID #{player.osu.uid}")
+            member.set_nick(player.osu.uname, "osu! user #{player.osu.uname} linked")
+          end
 
-        unless server.guest_role_id.nil? || player.country_code.nil? || player.country_code == 'IN'
-          member.add_role(server.guest_role_id, "Linked account's country is #{player.country_code}")
+          unless server.guest_role_id.nil? || player.country_code.nil? || player.country_code == 'IN'
+            member.add_role(server.guest_role_id, "Linked account's country is #{player.country_code}")
+          end
+        rescue
+          # This can fail if trying to set nicknames on admins so we just ignore it
         end
 
         @client
