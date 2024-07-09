d3edd39130f9f8dcc0dc125e3352747e8c5270a7
naoey <naoey@protonmail.ch>
2024-07-09 19:53:52 +0900

    Fix registration when Discord already exists due to exp

--- lib/discord/bot.rb
+++ lib/discord/bot.rb
@@ -207,8 +207,11 @@ module Discord
 
         player = Player.joins(:identities).find_by(identities: { provider: :discord, uid: author_id })
 
-        # Not giving any exp to users whose Discords aren't linked
-        return if player.nil?
+        if player.nil?
+          raw = DiscordHelper.identity_from_user(event.message.author)
+          player = Player.create(name: raw["username"])
+          PlayerAuth.create(provider: :discord, uid: author_id, uname: raw["username"], player:, raw:)
+        end
 
         exp = player.discord_exp.find_by(discord_server_id: server["id"])
 
