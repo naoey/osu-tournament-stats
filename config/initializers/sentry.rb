77c7d029c892dfe2b98069759eb0888351aaef68
naoey <naoey@protonmail.ch>
2025-05-10 19:12:34 +0900

    Disable Sentry outside production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,6 +2,7 @@ Sentry.init do |config|
   config.dsn = ENV.fetch("SENTRY_DSN", "")
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
   config.environment = ENV.fetch("RAILS_ENV", "development")
+  config.enabled_environments = %w[production]
 
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
