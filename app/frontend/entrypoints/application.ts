2ecd9af112d5e0fe590624794b73371af9cc3f75
naoey <naoey@protonmail.ch>
2025-05-17 20:01:37 +0900

    Add hack for stupid framework behaviour and fix main layout rendering

--- app/frontend/entrypoints/application.ts
+++ app/frontend/entrypoints/application.ts
@@ -3,7 +3,14 @@
 //
 //    <%= vite_client_tag %>
 //    <%= vite_javascript_tag 'application' %>
-console.log('Vite ⚡️ Rails')
+import { Component } from "react";
+import $ from "jquery";
+import ReactOnRails, { ReactComponentOrRenderFunction } from "react-on-rails";
+import { humanize, toTitleCase, transform } from "@alduino/humanizer/string";
+
+import "../stylesheets/application.scss";
+
+console.log("Vite ⚡️ Rails");
 
 // If using a TypeScript entrypoint file:
 //     <%= vite_typescript_tag 'application' %>
@@ -22,24 +29,42 @@ console.log('Vite ⚡️ Rails')
 // Example: Import a stylesheet in app/frontend/index.css
 // import '~/index.css'
 
-import { start as startTurbo } from '@hotwired/turbo'
-import $ from 'jquery';
-import ReactOnRails from 'react-on-rails';
-
-import "../stylesheets/application.scss";
-
 // startTurbo();
 
 (window as any).$ = (window as any).jQuery = $;
 
 const importContext = import.meta.glob("../components/**/*.{js,ts,tsx,jsx}", { eager: true });
-const componentsContext = {};
+const componentsContext: Record<string, ReactComponentOrRenderFunction> = {};
 
-Object.entries(importContext).forEach(([filename, component]) => {
+function normaliseComponentName(name: string): string {
+  return name;
+}
+
+const registeredNames = new Set<string>();
+
+Object.entries(importContext).forEach(([filename, module]) => {
   let cleanName = filename
     .replace("../components/", "")
     .replace(/\.\w+$/, ""); // Strips the file extension
-  componentsContext[cleanName] = Object.values(component)[0];
+
+  if (cleanName.endsWith("/index"))
+    cleanName = cleanName
+      .replace(/\/index$/, "");
+
+  const parts = cleanName.split("/");
+
+  while (parts.length > 2 && parts[parts.length - 1] === parts[parts.length - 2]) {
+    // Delete repeating segments
+    parts.splice(parts.length - 1, 1);
+  }
+
+  // This is a dirty workaround because ReactOnRails for some ridiculous reason changes the component name format from what is
+  // passed to `react_component()`. common/NavigationBar becomes Common::Navigationbar (???????????)
+  cleanName = parts.map(p => humanize(transform(p, toTitleCase)))
+    .join("::");
+
+  if (!registeredNames.has(cleanName))
+    componentsContext[cleanName] = Object.values(module as {})[0] as ReactComponentOrRenderFunction;
 });
 
 ReactOnRails.register(componentsContext);
